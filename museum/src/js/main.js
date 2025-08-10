b9d0493cbdf437aaae4e1113e2754d3c6b6e1a8c
Oksana Mirasova <mirasovaoksana@yandex.ru>
2025-08-10 19:26:04 +0700

    feat: add functionality for payment

--- museum/src/js/main.js
+++ museum/src/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".header__burger");
   const nav = document.querySelector(".header__nav");
   const wb = document.querySelector(".welcome__box");
+
   burger.addEventListener("click", function () {
     nav.classList.toggle("header__nav--open");
     burger.classList.toggle("header__burger--open");
@@ -69,6 +70,124 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     activeSlide(currentIndex)
   }
+
+  const openBtn = document.querySelector(".tickets__button");
+  const paymentSection = document.querySelector(".payment");
+  const closeBtn = document.querySelector(".payment__close");
+
+  openBtn.addEventListener("click", function () {
+    paymentSection.classList.toggle("payment--open");
+  });
+  closeBtn.addEventListener("click", function () {
+    paymentSection.classList.toggle("payment--open");
+  })
+
+
+
+  const dataInput = document.getElementById('data');
+  const timeInput = document.getElementById('time');
+  const typeInput = document.getElementById('type');
+  const basicInput = document.getElementById('basic');
+  const seniorInput = document.getElementById('senior');
+  const basicInputPayment = document.getElementById('basicPayment');
+  const seniorInputPayment = document.getElementById('seniorPayment');
+  const dataValue = document.querySelector(".payment__data-date")
+  const timeValue = document.querySelector(".payment__data-time")
+  const typeValue = document.querySelector(".payment__data-type")
+  const countBasic = document.querySelector('.payment__tiket-count-basic span');
+  const countSenior = document.querySelector('.payment__tiket-count-senior span');
+  const costBasic = document.querySelector('.payment__tiket-cost-basic span');
+  const costSenior = document.querySelector('.payment__tiket-cost-senior span');
+  const totalPrice = document.querySelector('.payment__total-price span');
+  const tiketsTotal = document.querySelector('.tickets__form-cost');
+
+
+
+
+  let ticketsTotalPrice = document.querySelector('.tickets__form-total span');
+
+  document.querySelectorAll('.tickets__count-box').forEach(countBlock => {
+    const minusBtn = countBlock.querySelector('.tickets__btn-minus');
+    const plusBtn = countBlock.querySelector('.tickets__btn-plus');
+    const input = countBlock.querySelector('input[type="number"]');
+
+    minusBtn.addEventListener('click', () => {
+      if (+input.value > +input.min) input.stepDown();
+      countPrise();
+    });
+
+    plusBtn.addEventListener('click', () => {
+      if (+input.value < +input.max) input.stepUp();
+      countPrise();
+    });
+    function countPrise() {
+      const priceBasic = Number(basicInput.value) * 20;
+      const priceSenior = Number(seniorInput.value) * 10;
+
+      ticketsTotalPrice.textContent = priceBasic + priceSenior;
+    }
+
+  });
+
+
+
+  openBtn.addEventListener('click', () => {
+    const basicValue = basicInput.value;
+    const seniorValue = seniorInput.value;
+    // Потом записать обратно в поля
+    basicInputPayment.value = basicValue;
+    seniorInputPayment.value = seniorValue;
+    countBasic.textContent = basicInputPayment.value;
+    countSenior.textContent = seniorInputPayment.value;
+    costBasic.textContent = basicInputPayment.value * 20
+    costSenior.textContent = seniorInputPayment.value * 10
+    totalPrice.textContent = Number(costSenior.textContent) + Number(costBasic.textContent)
+  });
+
+
+
+
+  dataInput.addEventListener('input', () => {
+    dataValue.textContent = dataInput.value;
+  });
+  timeInput.addEventListener('input', () => {
+    timeValue.textContent = timeInput.value;
+  });
+  typeInput.addEventListener('input', () => {
+    typeValue.textContent = typeInput.value;
+  });
+
+  document.querySelectorAll('.payment__count').forEach(countBlock => {
+    const minusBtn = countBlock.querySelector('.payment__count-minus');
+    const plusBtn = countBlock.querySelector('.payment__count-plus');
+    const input = countBlock.querySelector('input[type="number"]');
+
+    minusBtn.addEventListener('click', () => {
+      if (+input.value > +input.min) input.stepDown();
+      updateValue();
+    });
+
+    plusBtn.addEventListener('click', () => {
+      if (+input.value < +input.max) input.stepUp();
+      updateValue();
+    });
+
+  });
+
+  function updateValue() {
+    countBasic.textContent = basicInputPayment.value;
+    countSenior.textContent = seniorInputPayment.value;
+    costBasic.textContent = basicInputPayment.value * 20
+    costSenior.textContent = seniorInputPayment.value * 10
+    totalPrice.textContent = Number(costSenior.textContent) + Number(costBasic.textContent)
+  }
+
+
+
+
+
+
+
 });
 
 
